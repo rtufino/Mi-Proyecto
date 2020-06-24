550d6df5612f4c949bf0f3465e381b8623e9f2e0
Rodrigo Tufiño <rtufino@ups.edu.ec>
2020-06-23 23:04:27 -0500

    Commit inicial

---
+++ script.py
@@ -0,0 +1,11 @@
+# Script de prueba para Git
+# Autor: Rodrigo Tufiño <rtufino@ups.edu.ec>
+# Fecha: 23-06-2020
+# Version: 1.0.0
+
+def main():
+    print("Hola Mundo!")
+
+
+if __name__ == "__main__":
+    main()
